98aa96b1e14917a720da6f92dc6b75afd1c8021f
na <ady.niko51@gmail.com>
2017-12-01 18:53:54 +0200

    first commit

---
+++ Project4/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include "source1.cpp"
+
+int main()
+{
+	 signed char x;
+	 cin >> x;
+	printf("%d ",x);
+
+	system("pause");
+	return 0;
+}
---
+++ Project4/Source1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+void doi()
+{
+	int x;
+	x=25;
+}
